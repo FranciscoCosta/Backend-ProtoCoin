08846f5c72d1c1ee8c4400b91231a59211cf1258
FranciscoCosta <francisco100eg@gmail.com>
2024-06-30 17:07:40 -0300

    feat(helmet) : added validation backend and added helmet to protect verus basic attacks

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,46 @@
 import express , { Request, Response, NextFunction, json } from 'express';
 import dotenv from 'dotenv';
+import morgan from 'morgan';
+import helmet from 'helmet';
 
+import { mintAndTransfer } from './Web3Provider';
 dotenv.config();
 
 const PORT = process.env.PORT || 3000;
-import morgan from 'morgan';
 
-import { mintAndTransfer } from './Web3Provider';
+
 
 const app = express();
+import cors from 'cors';
+app.use(cors());
 
 app.use(morgan('dev'));
+
+app.use(helmet());
+
+app.use(cors({
+    origin: process.env.CORS_ORIGIN || '*'
+}));
 
 app.get('/', (req: Request, res: Response, next: NextFunction) => {
     res.send('Hello World');
 });
 
+
+const nextMint = new Map<string, number>();
+
+
 app.post("/mint/:wallet", async(req: Request, res: Response, next: NextFunction) => {
     try{
+        if(nextMint.has(req.params.wallet) && nextMint.get(req.params.wallet)! > Date.now()){
+            return res.status(429).send('You can mint only once a day.');
+        }
         const tx = await mintAndTransfer(req.params.wallet);
         res.send(tx);
-    }catch(err){
+    }catch(err: any){
         res.status(500).json(err);
+    }finally{
+        nextMint.set(req.params.wallet, Date.now() + 1000 * 60 * 60 * 24);
     }
 });
 
